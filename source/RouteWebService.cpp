fa293b8bf14f96810b7a5d1dfd378a52a28a6996
user <user@ubuntu.(none)>
2016-12-22 01:05:06 +0300

    mongo crud point with name

--- source/RouteWebService.cpp
+++ source/RouteWebService.cpp
@@ -10,11 +10,15 @@
 #include <bsoncxx/json.hpp>
 
 #include <mongocxx/client.hpp>
+#include <mongocxx/stdx.hpp>
+#include <mongocxx/uri.hpp>
 
 #include <iostream>
 #include <string>
 #include <sstream>
 #include <vector>
+#include <cstdint>
+#include <iterator>
 
 const std::string POST_METHOD = "POST";
 const std::string PUT_METHOD = "PUT";
@@ -22,6 +26,33 @@ const std::string GET_METHOD = "GET";
 const std::string DELETE_METHOD = "DELETE";
 
 const std::string NOT_FOUND = "NF";
+const std::string NO_CONTENT = "NC";
+const std::string NOT_MODIFIED = "NM";
+
+using bsoncxx::builder::stream::close_array;
+using bsoncxx::builder::stream::close_document;
+using bsoncxx::builder::stream::document;
+using bsoncxx::builder::stream::finalize;
+using bsoncxx::builder::stream::open_array;
+using bsoncxx::builder::stream::open_document;
+
+class UtilHelper {
+public:
+
+	static std::string join(const std::vector<std::string>& elements, const char* const separator) {
+    		switch (elements.size()) {
+        		case 0:
+            			return "";
+        		case 1:
+            			return elements[0];
+        		default:
+            			std::ostringstream os; 
+            			std::copy(elements.begin(), elements.end()-1, std::ostream_iterator<std::string>(os, separator));
+            			os << *elements.rbegin();
+            			return os.str();
+    		}
+	}
+};
 
 class RouteHandler {
 public:
@@ -37,13 +68,76 @@ public:
 class PointHandler {
 private:
 
+	std::string handlePut(const std::string& point_id, const fastcgi::DataBuffer& body) {
+		std::string request_body = "";
+		if (body.empty()) {
+			return NO_CONTENT;
+		}
+		
+		body.toString(request_body);
+		rapidjson::Document doc_body;
+		doc_body.Parse(request_body.c_str());
+		const std::string new_name = doc_body["name"].GetString();
+
+		mongocxx::client conn{mongocxx::uri{}};
+    		auto collection = conn["route_service"]["points"];
+		const auto& result = collection.update_one(document{} << "name" << point_id << finalize,
+                      document{} << "$set" << open_document <<
+                        "name" << new_name << close_document << finalize);
+
+		if (result && (*result).matched_count() > 0) {
+			mongocxx::stdx::optional<bsoncxx::document::value> result_check = collection.find_one(document{} << "name" << new_name << finalize);
+    			if (result_check) {
+				return bsoncxx::to_json(*result_check);
+			}
+			return NOT_MODIFIED;
+		}
+
+		return NOT_FOUND;
+	}
+
+	std::string handleGet(const std::string& point_id) {
+		mongocxx::client conn{mongocxx::uri{}};
+    		auto collection = conn["route_service"]["points"];
+		mongocxx::stdx::optional<bsoncxx::document::value> result = collection.find_one(document{} << "name" << point_id << finalize);
+    		if (result) {
+			return bsoncxx::to_json(*result);
+		}
+		return NOT_FOUND;
+	}
+
+	std::string handleDelete(const std::string& point_id) {
+		mongocxx::client conn{mongocxx::uri{}};
+    		auto collection = conn["route_service"]["points"];
+		const auto& result = collection.delete_one(document{} << "name" << point_id << finalize);
+    		if (result && (*result).deleted_count() > 0) {
+			return "";
+		}
+		return NOT_FOUND;
+	}
+
+	std::string handleMethod(const std::string& method, const std::string& point_id, const fastcgi::DataBuffer& body) {
+		if (method.compare(GET_METHOD) == 0) {
+			return handleGet(point_id);
+		}
+		if (method.compare(PUT_METHOD) == 0) {
+			return handlePut(point_id, body);
+		}
+		if (method.compare(DELETE_METHOD) == 0) {
+			return handleDelete(point_id);
+		}
+		return NOT_FOUND;
+	}
+
 public:
 	PointHandler() {}
 	~PointHandler() {}
 
 	std::string handle(const std::string& method, const std::vector<std::string> &path, const fastcgi::DataBuffer& body) {
-		std::string result = "Hello from point\n";
-		return result;
+		if (path.size() != 2) {
+			return NOT_FOUND;
+		}
+		return handleMethod(method, path[1], body);
 	}
 };
 
@@ -63,8 +157,7 @@ private:
 	std::string handlePost(const fastcgi::DataBuffer& body) {
 		std::string result = "";
 		if (body.empty()) {
-			result = "Empty\n";
-			return result;
+			return NO_CONTENT;
 		}
 		body.toString(result);
 		
@@ -72,17 +165,39 @@ private:
 		doc_body.Parse(result.c_str());
 		
 		mongocxx::client conn{mongocxx::uri{}};
-    		bsoncxx::builder::stream::document point{};
+    		document point{};
     		auto collection = conn["route_service"]["points"];
-    		point << "name" << doc_body["name"].GetString();
-    		collection.insert_one(point.view());
 
-		//result = doc_body["name"].GetString();
-		return result;
+		mongocxx::stdx::optional<bsoncxx::document::value> check_exist = collection.find_one(document{} << "name" << doc_body["name"].GetString() << finalize);
+    		if (check_exist) {
+			result = bsoncxx::to_json(*check_exist);
+			return result;
+		}
+
+		point << "name" << doc_body["name"].GetString();
+    		collection.insert_one(point.view());
+		mongocxx::stdx::optional<bsoncxx::document::value> check_result = collection.find_one(document{} << "name" << doc_body["name"].GetString() << finalize);
+		if (check_result) {
+			result = bsoncxx::to_json(*check_result);
+			return result;
+		}
+
+		return NOT_MODIFIED;
 	}
 
 	std::string handleGet() {
-		std::string result = "Hello from points get\n";
+		std::string result = "{\"points\":[";
+
+		mongocxx::client conn{mongocxx::uri{}};
+    		auto collection = conn["route_service"]["points"];
+		mongocxx::cursor cursor = collection.find(document{} << finalize);
+		std::vector<std::string> str_points;
+		for(const auto& doc : cursor) {
+  			str_points.push_back(bsoncxx::to_json(doc));
+		}
+
+		result.append(UtilHelper::join(str_points, ","));
+		result.append("]}");
 		return result;
 	}
 
@@ -157,8 +272,7 @@ public:
 	}
 	std::string handle(const std::string& method, const std::vector<std::string> &path, const fastcgi::DataBuffer& body) {
 		if (path.size() == 0) {
-			std::string res = "Hello from root\n";
-			return res;
+			return NOT_FOUND;
 		}
 		std::string points = "points", relations = "relations", route = "route";
 		if (path[0].compare(points) == 0) {
@@ -191,9 +305,12 @@ public:
 		
 		if (result.compare(NOT_FOUND) == 0) {
 			request->setStatus(404);
-		}
-		else {
-			//request->setContentType("application/json");
+		} else if (result.compare(NO_CONTENT) == 0) {
+			request->setStatus(204);
+		} else if (result.compare(NOT_MODIFIED) == 0) {
+			request->setStatus(304);
+		} else {
+			request->setContentType("application/json");
 			request->write(result.c_str(), result.length());
 		}		
                 //request->setHeader("Route-Web-Service-Header", "Reply from HelloWorld");
